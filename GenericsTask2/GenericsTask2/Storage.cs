898d140b88ec1bd60114719c2a699d5ce13b986c
SparkCode <iseb@outlook.com>
2016-04-11 16:54:01 +0400

    solve

---
+++ GenericsTask2/GenericsTask2/Storage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericsTask2
+{
+    class Worker
+    {
+        private readonly Dictionary<Guid, object> _storage;
+
+        public Worker()
+        {
+            _storage = new Dictionary<Guid, object>();
+        }
+
+        public T Add<T>() where T : class, new()
+        {
+            var @object = new T();
+            _storage.Add(Guid.NewGuid(), @object);
+            return @object;
+        }
+
+        public IEnumerable<Tuple<Guid, T>> GetTuplesByType<T>() where T : class, new() => 
+            _storage
+            .Where(x => x.Value is T)
+            .Select(x => new Tuple<Guid,T>(x.Key, (T)x.Value))
+            .ToList();
+
+        public T GetObjectById<T>(Guid id) where T : class, new() => 
+            _storage
+            .Where(x => x.Key == id && x.Value is T)
+            .Select(x => (T)x.Value)
+            .FirstOrDefault();
+    }
+}
---
+++ GenericsTask2/GenericsTask2/Test.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace GenericsTask2
+{
+    [TestFixture]
+    class Test
+    {
+        [Test]
+        public void IfTypeAddedOnlyOnceNumberThisInObject1EqualOne()
+        {
+            var worker = new Worker();
+            worker.Add<Random>();
+
+            var res = worker.GetTuplesByType<Random>();
+            Assert.That(res.Count() == 1);
+        }
+
+        [Test]
+        public void IfTwoTypesAddedOnlyOnceNumberEachInObject1EqualOne()
+        {
+            var worker = new Worker();
+            worker.Add<Random>();
+            worker.Add<List>();
+
+            var randomTyples = worker.GetTuplesByType<Random>();
+            var listTyples = worker.GetTuplesByType<Random>();
+
+            Assert.That(randomTyples.Count() == 1 && listTyples.Count() == 1);
+        }
+
+        //[Test]
+        //public void Test2()
+        //{
+        //    var worker = new Worker();
+
+        //    worker.Add<Random>();
+        //    var tuples = worker.GetTuplesByType<Random>().ToList();
+        //    var id = tuples.First().Item1;
+        //    var @object = tuples.First().Item2;
+        //    var randomObject = worker.GetObjectById<Random>(id);
+
+        //    Assert.That(randomObject != null && tuples.Count() == 1 && @object == randomObject);
+        //}
+    }
+}
